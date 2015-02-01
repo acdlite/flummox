9d2a7210028cc27e4cea94e1d605993114274802
Andrew Clark <acdlite@me.com>
2015-01-31 20:10:56 -0500

    Add Promise polyfill for tests

--- src/test-init.js
+++ src/test-init.js
@@ -3,5 +3,8 @@
 import chai from 'chai';
 global.expect = chai.expect;
 
+import { Promise } from 'es6-promise';
+if (!global.Promise) global.Promise = Promise;
+
 import sourceMapSupport from 'source-map-support';
 sourceMapSupport.install();
