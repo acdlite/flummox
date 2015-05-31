5d1ef6e108fbfed3c32ddf3ed2a9343d9a390d93
Johannes Lumpe <johannes@johanneslumpe.de>
2015-05-31 11:07:44 +0300

    Converted exports to default exports for easier usage in browser export

--- src/addons/react.js
+++ src/addons/react.js
@@ -3,5 +3,7 @@ import React from 'react/addons';
 import createFluxComponent from './FluxComponent';
 import createConnect from './connect';
 
-export const FluxComponent = createFluxComponent(React, 'span');
-export const connect = createConnect(React);
+export default {
+  FluxComponent: createFluxComponent(React, 'span'),
+  connect: createConnect(React)
+};
